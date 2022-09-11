81600da5c96993f69b8b8ac5013f04dd9403512c
Karina <ivettra@mail.ru>
2022-09-11 13:47:59 +0500

    feat: delete method

--- backend/routes/vacancies.js
+++ backend/routes/vacancies.js
@@ -1,47 +1,49 @@
 // это файл маршрутов
-const router = require('express')
-	.Router(); // создали роутер
+const router = require("express").Router(); // создали роутер
+
+const { celebrate, Joi } = require("celebrate");
 
 const {
-	celebrate,
-	Joi,
-} = require('celebrate');
+  getVacancies,
+  createVacancies,
+  deleteVacancy,
+} = require("../controllers/vacancies");
 
-const {
-	getVacancies,
-	createVacancies
-} = require('../controllers/vacancies');
+const urlRegExp = new RegExp(
+  "^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w.-]+)+[\\w\\-._~:/?#[\\]@!$&'()*+,;=.]+$"
+);
 
-const urlRegExp = new RegExp('^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w.-]+)+[\\w\\-._~:/?#[\\]@!$&\'()*+,;=.]+$');
+router.get("/vacancies", getVacancies);
 
-router.get('/vacancies', getVacancies);
+router.post(
+  "/vacancies",
+  celebrate({
+    // валидируем параметры
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      salary: Joi.number().required(),
+      education: Joi.string().required(),
+      experience: Joi.string().required(),
+      image: Joi.string().required().pattern(urlRegExp),
+      updatedBy: Joi.string().required(),
+      workload: Joi.string().required(),
+      description: Joi.string().required(),
+      conditions: Joi.array().items(Joi.string().required()),
+      phoneNumber: Joi.string().required(),
+    }),
+  }),
+  createVacancies
+);
 
-router.post('/vacancies', celebrate({
-	// валидируем параметры
-	body: Joi.object()
-		.keys({
-			name: Joi.string()
-				.required(),
-			salary: Joi.number()
-				.required(),
-			education: Joi.string()
-				.required(),
-			experience: Joi.string()
-				.required(),
-			image: Joi.string()
-				.required()
-				.pattern(urlRegExp),
-			updatedBy: Joi.string()
-				.required(),
-			workload: Joi.string()
-				.required(),
-			description: Joi.string()
-				.required(),
-			conditions: Joi.array()
-        .items(Joi.string().required()),
-			phoneNumber: Joi.string()
-				.required(),
-		}),
-}), createVacancies);
+router.delete(
+  "/vacancies/:vacancyId",
+  celebrate({
+    // валидируем параметры
+    params: Joi.object().keys({
+      vacancyId: Joi.string().required().length(24).hex(),
+    }),
+  }),
+  deleteVacancy
+);
 
 module.exports = router;
